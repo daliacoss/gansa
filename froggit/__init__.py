4d1660c321f1ab640362624c8a8fbf3c76294924
Decky Coss <coss@cosstropolis.com>
2016-01-05 00:42:24 -0500

    added init command; changed environment directory structure

--- froggit/__init__.py
+++ froggit/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import os, imp, collections, shutil
+import os, imp, collections, shutil, csv
 import jinja2, markdown, yaml
 from . import six
 from markdown.extensions.meta import MetaExtension
@@ -13,10 +13,10 @@ TMP_TEMPLATE = """
 {{% endblock %}}
 """
 
-def deep_update(dict1, dict2):
+def _deep_update(dict1, dict2):
 	for k, v in dict2.items():
 		if isinstance(v, collections.Mapping):
-			r = deep_update(dict1.get(k, {}), v)
+			r = _deep_update(dict1.get(k, {}), v)
 			dict1[k] = r
 		else:
 			dict1[k] = v
@@ -25,33 +25,113 @@ def deep_update(dict1, dict2):
 class Site(object):
 
 	default_settings = {
+		"database": {},
 		"environment": {
 			"views": "views.yaml",
 			"pages": "pages",
 			"templates": "templates",
-			"assets": "assets"
+			"assets": "assets",
+			"database": "db.yaml"
 		},
 		"default_block": "content"
 	}
 
-	def __init__(self, environment="."):
+	def __init__(self, environment, load=True):
 
 		self.environment = os.path.abspath(environment)
 		self.settings = dict(self.default_settings)
+		self.views = []
+
+		if not load or not os.path.exists(self.environment_src):
+			return
+
 		try:
-			with open(os.path.join(self.environment, "settings.yaml")) as settings_file:
-				settings = yaml.load(settings_file)
-				deep_update(self.settings, settings)
-		except IOError:
+			self.load_environment()
+		except IOError, OSError:
 			pass
 
-		self.templates = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.join(self.environment, "templates")))
-		with open(os.path.join(self.environment, self.settings["environment"]["views"])) as vstream:
-			self.views = yaml.load(vstream)
-		self.db = {}
+	def load_environment(self):
 
+		self.load_settings()
+		self.load_templates()
+		self.load_views()
+		self.load_db()
+
+	def load_db(self):
+
+		db_engine = self.settings["database"].get("engine")
+		if not db_engine:
+			return
+
+		if db_engine in ["yaml", "csv"]:
+			with open(os.path.join(self.environment_src, self.settings["environment"]["database"])) as stream:
+				if db_engine == "yaml":
+					self.db = yaml.load(stream) or {}
+				else:
+					self.db = csv.reader(stream)
+
+	def load_templates(self):
+
+		template_loader = jinja2.FileSystemLoader(os.path.join(self.environment, self.settings["environment"]["templates"]))
+		self.templates = jinja2.Environment(cache_size=0, loader=template_loader)
+
+	def load_views(self):
+
+		with open(os.path.join(self.environment_src, self.settings["environment"]["views"])) as vstream:
+			self.views = yaml.load(vstream) or []
 		self.set_full_routes()
 		self.set_templates()
+
+	def load_settings(self, merge=True):
+		"""
+		load settings from this project's settings.yaml file
+
+		parameters:
+			merge=True: attempt to merge settings with already loaded settings
+		"""
+
+		with open(os.path.join(self.environment_src, "settings.yaml")) as settings_file:
+			settings = yaml.load(settings_file)
+			_deep_update(self.settings, settings)
+
+		db = self.settings["environment"]["database"]
+
+		if not db:
+			return
+
+		# attempt to infer db engine from file name, if one is not specified
+		self.settings["database"]["engine"] = self.settings["database"].get("engine") or {
+			"yaml": "yaml",
+			"csv": "csv",
+			"sqlite": "sqlite",
+			"db": "sqlite"
+		}.get(db.split(".")[-1])
+
+	def init_environment(self):
+
+		for d in [
+			self.environment_src,
+			os.path.join(self.environment_src, "assets"),
+			os.path.join(self.environment_src, "pages"),
+			os.path.join(self.environment_src, "templates"),
+			os.path.join(self.environment, "distribute")
+		]:
+			if not os.path.exists(d):
+				os.mkdir(d)
+
+		with open(os.path.join(self.environment_src, "settings.yaml"), "w") as settings_file:
+			yaml.dump(self.settings, settings_file, default_flow_style=False)
+
+		with open(os.path.join(self.environment_src, "views.yaml"), "w") as views_file:
+			pass
+
+		db_filename = self.settings["environment"]["database"]
+		with open(os.path.join(self.environment_src, db_filename), "w") as views_file:
+			pass
+
+	@property
+	def environment_src(self):
+		return os.path.join(self.environment, "src")
 
 	@property
 	def routes(self):
@@ -75,6 +155,7 @@ class Site(object):
 		"""set full routes for each view"""
 
 		views = views or self.views
+		print(self.views)
 
 		for view in views:
 			view["full_route"] = route_prefix + view["route"]
@@ -116,6 +197,19 @@ class Site(object):
 
 		md = markdown.Markdown(extensions=[MetaExtension()])
 
+		#copy assets (skip if this is not the top level of the recursive build)
+		if views == self.views:
+			try:
+				assets_folder = os.path.join(self.environment_src, self.settings["environment"]["assets"])
+				for f in os.listdir(assets_folder):
+					if os.path.isdir(os.path.join(assets_folder, f)):
+						shutil.copytree(os.path.join(assets_folder, f), os.path.join(out, f))
+					else:
+						shutil.copy(os.path.join(assets_folder, f), out)
+			except OSError:
+				print("Could not copy assets")
+		tmp_fname  = os.path.join(self.environment_src, "templates", "_tmp.html")
+
 		for view in views:
 			if view.get("subviews"):
 				new_out = os.path.join(out, view["route"])
@@ -134,6 +228,7 @@ class Site(object):
 				self.settings["environment"]["pages"],
 				fname
 			) for fname in page_fnames]
+
 
 			with open(os.path.join(out, view["route"]), "w") as out_file:
 				try:
@@ -164,23 +259,17 @@ class Site(object):
 							blocks[block_name] = html
 							tmp_template += "{{% block {0}%}}{1}{{% endblock %}}".format(block_name, html)
 
-					with open(os.path.join(self.environment, "templates", "_tmp.html"), "w") as tmp:
+					with open(tmp_fname, "w") as tmp:
 						tmp.write(tmp_template)
+
 					template = self.templates.get_template("_tmp.html")
 					out_file.write(template.render(**context))
-				except IOError:
+				except OSError:
 					template = self.templates.get_template(view["template"])
-					out_file.write(template.render(**context))
+					out_file.write(template.render(**context).encode('utf8'))
 
-		#skip asset copying if this is not the top level of the recursive build
-		if views != self.views:
-			return
-		try:
-			assets_folder = os.path.join(self.environment, self.settings["environment"]["assets"])
-			for f in os.listdir(assets_folder):
-				if os.path.isdir(os.path.join(assets_folder, f)):
-					shutil.copytree(os.path.join(assets_folder, f), os.path.join(out, f))
-				else:
-					shutil.copy(os.path.join(assets_folder, f), out)
-		except OSError:
-			pass
+				if views == self.views:
+					try:
+						os.remove(tmp_fname)
+					except OSError:
+						pass
